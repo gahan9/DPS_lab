28a722f18d9501418851806ed0bc4b6f098ad16c
gahan9 <gahansaraiya@gmail.com>
2019-01-20 23:06:24 +0530

    shell script to run commands

--- practical1/src/execute_parallel.sh
+++ practical1/src/execute_parallel.sh
@@ -1,5 +1,13 @@
-gcc -pg -fopenmp main.c -o vector_add_sequential
-time ./vector_add_sequential
-gprof vector_add_sequential gmon.out > vector_add_sequential.txt
+# gcc -pg -fopenmp main.c -o vector_add_sequential
+# time ./vector_add_sequential
+# gprof vector_add_sequential gmon.out > vector_add_sequential.txt
 
-gcc -fopenmp main.c -o vector_add_parallel
+# gcc -fopenmp main.c -o vector_add_parallel
+
+
+# Sequential
+gcc -pg -fopenmp main.c -o vector_add_sequential &&time ./vector_add_sequential
+
+
+# Paralllel
+gcc -pg -fopenmp vectorAddParallel.c -o vector_add_parallel && time ./vector_add_parallel
