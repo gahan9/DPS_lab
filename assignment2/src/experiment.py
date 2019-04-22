5550f3bc41b60bddabb5e38573c42f6483093992
gahan9 <gahansaraiya@gmail.com>
2019-04-22 08:53:07 +0530

    updated doc and experiment with observation

--- assignment2/src/experiment.py
+++ assignment2/src/experiment.py
@@ -5,22 +5,22 @@ Author: Gahan Saraiya
 GiT: https://github.com/gahan9
 StackOverflow: https://stackoverflow.com/users/story/7664524
 
-Experiment of loop of n^3 complexity without Ray
+Experiment of loop of n^2 complexity without Ray
 """
 
 import time
 
+
 def stress_function(num):
-    return sum([i*j*1 
-                for i in range(num) 
-                for j in range(num) 
+    return sum([i * j * 1
+                for i in range(num)
+                for j in range(num)
                 for k in range(1)
                 ])
 
 
 if __name__ == "__main__":
-    # TEST_LIS = [100, 200, 300, 500]
-    TEST_LIS = [700, 1000]
+    TEST_LIS = [100, 200, 300, 500, 700, 1000]
     for t in TEST_LIS:
         start = time.time()
         result = [stress_function(t) for _ in range(t)]
--- assignment2/src/experimentRay.py
+++ assignment2/src/experimentRay.py
@@ -5,26 +5,27 @@ Author: Gahan Saraiya
 GiT: https://github.com/gahan9
 StackOverflow: https://stackoverflow.com/users/story/7664524
 
-Experiment of loop of n^3 complexity with Ray
+Experiment of loop of n^2 complexity with Ray
 """
 import time
 import ray
 
+
 @ray.remote
 def stress_function(num):
     # Example of loop O(n^2)
-    return sum([i*j*1 
-                for i in range(num) 
-                for j in range(num) 
+    return sum([i * j * 1
+                for i in range(num)
+                for j in range(num)
                 for k in range(1)
-    ])
+                ])
+
 
 if __name__ == "__main__":
     # Initialize ray
     ray.init()
     # ray.init(redis_address="0.0.0.0:6667")
-    # TEST_LIS = [100, 200, 300, 500]
-    TEST_LIS = [700, 1000]
+    TEST_LIS = [100, 200, 300, 500, 700, 1000]
     for t in TEST_LIS:
         start = time.time()
         # Remote function is invoked by .remote keyword
