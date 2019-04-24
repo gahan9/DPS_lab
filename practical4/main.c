fc4755d29743fbfe2e504465b32407e30cedc7db
mpi <gahansaraiya@gmail.com>
2019-04-24 16:36:58 +0530

    added sample input and code definition

---
+++ practical4/main.c
@@ -0,0 +1,14 @@
+// --------------------------------------------------------------------------------------------
+// Author: Gahan Saraiya
+// GiT: http://github.com/gahan9/
+// StackOverflow: https://stackoverflow.com/users/story/7664524
+// Website: http://gahan9.github.io/
+// --------------------------------------------------------------------------------------------
+// Practical-4
+// -----------
+// Input: Year, amount
+// ODD-Group (max amount)
+// Find year with highest total amount (summation of all order in Year)
+// EVEN-Group (number of order)
+// Find year with highest number of order placed (count of all order in Year)
+
